19a0c4c9949df7ad6ed9f6a81839f30b2d74d776
AlanDReeves <alandanielreeves@gmail.com>
2025-02-13 16:24:39 -1000

    Created GUI prototype
    Prototype actually functions this time.

---
+++ ramanDataProcessing/RamanGUI.py
@@ -0,0 +1,128 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QFileDialog, QListWidget, QSpinBox, QLabel, QSlider
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtCore import Qt
+import pyqtgraph as pg
+from RamanProcessor import RamanProcessor
+
+app = QApplication([])
+
+class newMainWindow(QMainWindow):
+    #contains constructor and effectively is the main function
+    def __init__(self, parent = ..., flags = ...):
+        # set up
+        super().__init__()
+        self.setWindowTitle("Raman GUI")
+        layout = QVBoxLayout()
+        self.setMinimumSize(800, 600)
+
+        self.fileSelected = False
+        self.ramanFile: str = ''
+        self.backend: RamanProcessor = None
+
+        #internal devices
+        quitButton = QPushButton("Close Program")
+        quitButton.setCheckable(True)
+        layout.addWidget(quitButton)
+
+        fileSelectorButton = QPushButton("Select A File")
+        fileSelectorButton.setCheckable(True)
+        layout.addWidget(fileSelectorButton)
+
+        makePyPlotButton = QPushButton("Make PyPlot")
+        makePyPlotButton.setCheckable(True)
+        layout.addWidget(makePyPlotButton)
+
+        peakList = QListWidget()
+        peakList.addItem("Peaks List")
+        peakList.addItem("X val, Y val")
+        layout.addWidget(peakList)
+
+        self.peakSelectorA = QSpinBox()
+        self.peakSelectorA.setPrefix("Peak Number: ")
+        self.peakSelectorA.setMinimum(0)
+        self.peakSelectorA.setMaximum(10)
+        layout.addWidget(self.peakSelectorA)
+        
+        self.peakSelectorB = QSpinBox()
+        self.peakSelectorB.setPrefix("Peak Number: ")
+        self.peakSelectorB.setMinimum(0)
+        self.peakSelectorB.setMaximum(10)
+        layout.addWidget(self.peakSelectorB)
+
+        self.ratioLabel = QLabel("Ratio of peak A to peak B: ")
+        layout.addWidget(self.ratioLabel)
+
+        normalizerButton = QPushButton("Normalize Data")
+        normalizerButton.setCheckable(True)
+        layout.addWidget(normalizerButton)
+
+        self.screenSlider = QSlider(Qt.Horizontal)
+        self.screenSlider.setMinimum(20)
+        self.screenSlider.setMaximum(250)
+        layout.addWidget(self.screenSlider)
+
+        sliderLabel = QLabel("Slider Setting: " + str(self.screenSlider.value()))
+        layout.addWidget(sliderLabel)
+
+
+        
+        #button functions
+
+        def openFileBrowser():
+            #selects a file, creates a RamanProcessor object, adds peaks found to list, and limits peak selector boxes
+            options = QFileDialog.Options()
+            filePath, _ = QFileDialog.getOpenFileName(self, "Select a File", "", "All Files (*);;Text Files (*.txt)", options=options)
+            self.ramanFile = filePath
+            self.fileSelected = True
+            self.backend = RamanProcessor(self.ramanFile)
+            i = int(0)
+            for element in self.backend.peaksList:
+                peakList.addItem("Peak " + str(i) + ": " + str(element[0]) + ', ' + str(element[1]))
+                i += 1
+            self.peakSelectorA.setMaximum(len(self.backend.peaksList) - 1)
+            self.peakSelectorB.setMaximum(len(self.backend.peaksList) - 1)
+
+        def makePyPlot():
+            if (self.fileSelected == True):
+                self.backend.makePlot()
+
+        def comparepeaks():
+            if (self.fileSelected == True):
+                peak1Val = self.backend.peaksList[int(self.peakSelectorA.value())][1]
+                peak2Val = self.backend.peaksList[int(self.peakSelectorB.value())][1]
+                ratio = self.backend.comparePeaks(peak1Val, peak2Val)
+                self.ratioLabel.setText("Ratio of peak A to peak B: " + str(ratio))
+            else:
+                pass
+
+        def normalizeData():
+            if (self.fileSelected == True):
+                self.backend.normalizeYAxis()
+
+        def changeScreenSize():
+            if (self.fileSelected == True):
+                self.backend.setScreenSize(self.screenSlider.value())
+                sliderLabel.setText("Slider Setting: " + str(self.screenSlider.value()))
+        
+        # connecting buttons to functions
+        quitButton.clicked.connect(self.close)
+        fileSelectorButton.clicked.connect(openFileBrowser)
+        makePyPlotButton.clicked.connect(makePyPlot)
+        self.peakSelectorA.valueChanged.connect(comparepeaks)
+        self.peakSelectorB.valueChanged.connect(comparepeaks)
+        normalizerButton.clicked.connect(normalizeData)
+        self.screenSlider.valueChanged.connect(changeScreenSize)
+
+        #setting "widget", which holds all other devices, as the central widget
+        holderWidget = QWidget()
+        holderWidget.setLayout(layout)
+
+        self.setCentralWidget(holderWidget)
+
+
+
+window = newMainWindow()
+window.show()
+app.exec()
+
+# TODO: Add something to display the plot inside GUI using pyqtgraph
